859f5b9956ce6be04d85a437b0007a84979e45ee
Ardy <116569704+0xArdy@users.noreply.github.com>
2025-10-27 15:46:18 -0400

    fix: route item hover color (#95)

--- src/web/modules/PPv1/screens/dashboard/components/Routes/RouteItem/RouteItem.tsx
+++ src/web/modules/PPv1/screens/dashboard/components/Routes/RouteItem/RouteItem.tsx
@@ -92,7 +92,7 @@ const RouteItem: FC<Props> = ({ routeItem, index, routeItemsLength }) => {
                   themeType === THEME_TYPES.DARK
                     ? theme.primary
                     : hovered
-                    ? '#c197ff'
+                    ? theme.infoDecorative
                     : theme.primaryBackground
                 }
                 height={ITEM_HEIGHT}
