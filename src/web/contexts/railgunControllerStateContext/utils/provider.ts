0ffa2d9f6a191f2bcce081210fc0f623825ace98
Karen Sarkisyan <karen.sarkisyan01@gmail.com>
2026-03-03 12:10:04 +0100

    feat: verify railgun logs when using Helios as a provider

--- src/web/contexts/railgunControllerStateContext/utils/provider.ts
+++ src/web/contexts/railgunControllerStateContext/utils/provider.ts
@@ -1,18 +1,38 @@
 import { testnetNetworks } from '@ambire-common/consts/testnetNetworks'
 import { RAILGUN_CONFIG_BY_CHAIN_ID } from '@kohaku-eth/railgun'
 import { JsonRpcProvider, Network } from 'ethers'
+import { getRpcProviderForUI } from '@web/services/provider/getRpcProviderForUI'
+import { UIProxyProvider } from '@web/services/provider/UIProxyProvider'
 
-export const getProvider = (chainId: number) => {
+export type RailgunProviders = {
+  logsProvider: JsonRpcProvider
+  verificationProvider: UIProxyProvider | null
+}
+
+export const getProvider = (chainId: number, dispatch: (action: any) => void): RailgunProviders => {
   const name =
     RAILGUN_CONFIG_BY_CHAIN_ID[chainId.toString() as keyof typeof RAILGUN_CONFIG_BY_CHAIN_ID].NAME
   const networkConfig = testnetNetworks.find((n) => n.chainId === BigInt(chainId))
   if (!name || !networkConfig) {
     throw new Error(`Unsupported chainId for Railgun: ${chainId}`)
   }
-  return new JsonRpcProvider(networkConfig.selectedRpcUrl, Network.from({ name, chainId }), {
-    staticNetwork: true,
-    batchMaxCount: 1,
-    batchMaxSize: 0,
-    batchStallTime: 0
-  })
+
+  const logsProvider = new JsonRpcProvider(
+    networkConfig.selectedRpcUrl,
+    Network.from({ name, chainId }),
+    {
+      staticNetwork: true,
+      batchMaxCount: 1,
+      batchMaxSize: 0,
+      batchStallTime: 0
+    }
+  )
+
+  if (networkConfig.rpcProvider !== 'helios') {
+    return { logsProvider, verificationProvider: null }
+  }
+
+  const verificationProvider = getRpcProviderForUI(networkConfig, dispatch)
+
+  return { logsProvider, verificationProvider }
 }
